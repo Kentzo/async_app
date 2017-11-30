8ec4ce5dd7df07ee82c1a4993c3324896664084f
Ilya Kulakov <kulakov.ilya@gmail.com>
2017-11-29 17:05:47 -0800

    Expose the make_service decorator at the top.

--- async_app/__init__.py
+++ async_app/__init__.py
@@ -1,4 +1,4 @@
 from .__about__ import __version__
 
-from .app import App, Runnable, Service
+from .app import App, Runnable, Service, make_service
 from .config import ChainConfig, Config, Option, ConfigOption
