7af3d60187cfa2b1a174e27bd26de861fa54d6b5
Ilya Kulakov <kulakov.ilya@gmail.com>
2017-12-06 13:57:52 -0800

    Make wait_one, TaskGroup and AsyncExitStack top-level objects.

--- async_app/__init__.py
+++ async_app/__init__.py
@@ -2,3 +2,4 @@ from .__about__ import __version__
 
 from .app import App, Runnable, Service
 from .config import ChainConfig, Config, Option, ConfigOption
+from .utils import wait_one, TaskGroup, AsyncExitStack
